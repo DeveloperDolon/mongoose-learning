d54d441a17c2b9f0f7358b4ee2e44aceb859714c
Dolon Chandra Roy <dolonr718@gmail.com>
2024-05-15 21:15:51 +0600

    project setup complete commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { Application, Request, Response } from 'express';
+import express from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  console.log(a);
+  res.send('Hello World!');
+});
+
+export default app;
