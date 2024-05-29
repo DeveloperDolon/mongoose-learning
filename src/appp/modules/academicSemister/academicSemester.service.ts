27a7f75ba0def40a278fbde22dd2a6051c95e7a6
Dolon Chandra Roy <dolonr718@gmail.com>
2024-05-30 04:38:56 +0600

    practise task starting commit

--- src/appp/modules/academicSemister/academicSemester.service.ts
+++ src/appp/modules/academicSemister/academicSemester.service.ts
@@ -1,18 +1,8 @@
 import { AcademicSemesterModel } from './academic.model';
+import { academicSemesterNameCodeMapper } from './academicSemester.constant';
 import { TAcademicSemester } from './academicSemister.interface';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
-  type TAcademicSemesterNameCodeMapper = {
-    [key: string]: string;
-  };
-
-  // semester name ====> semester code
-  const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
-    Autumn: '01',
-    Summer: '02',
-    Fall: '03',
-  };
-
   if (academicSemesterNameCodeMapper[payload?.name] !== payload?.code) {
     throw new Error('Invalid semester code...!');
   }
